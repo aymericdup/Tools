2e093a8b9d735197a5c2fc5111808ee23c35b23e
NerthusFinance <julien.revelle@nerthus-finance.fr>
2016-06-30 11:15:06 +0200

    Add class GetWebPageContent - synchrone

---
+++ IndexKor/IndexKor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IndexKor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string webPageContent = WebTools.HtmlContent.WebPageContent.GetWebPageContent("https://finance.yahoo.com/q/cp?s=%5EFCHI");
+                Console.Write(webPageContent);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebTools/WebTools/HtmlContent/WebPageContent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebTools.HtmlContent
+{
+    public static class WebPageContent
+    {
+        static private string GetPlainText(string url)
+        {
+            StringBuilder sb = new StringBuilder();
+            WebBrowser webBrowser = new WebBrowser();
+            webBrowser.ScriptErrorsSuppressed = true;
+            webBrowser.Url = new Uri(url);
+            while (webBrowser.ReadyState != WebBrowserReadyState.Complete)
+                Application.DoEvents();
+            webBrowser.Document.ExecCommand("SelectAll", false, null);
+            webBrowser.Document.ExecCommand("Copy", false, null);
+            webBrowser.Dispose();
+            return Clipboard.GetText();
+        }
+
+        static public string GetWebPageContent(string url, bool isThreadSafeMethodNeeded = true)
+        {
+            string content = string.Empty;
+            Thread thread = new Thread(() =>
+            {
+                content = GetPlainText(url);
+            });
+            if (isThreadSafeMethodNeeded)
+                thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            return content;
+        }
+    }
+}
