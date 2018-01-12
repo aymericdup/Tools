214066f9300411e36ac688737707534baf31c38a
aymericdup <aymeric.dup@gmail.com>
2018-01-12 19:32:47 +0100

    - Create ArbMaker - Parse Betclic football

---
+++ ArbMaker/ArbMaker/ParserFrm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ArbMaker
+{
+    partial class ParserFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webPageContentTb = new System.Windows.Forms.TextBox();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.urlTb = new System.Windows.Forms.TextBox();
+            this.footballBetclicBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webPageContentTb
+            // 
+            this.webPageContentTb.AllowDrop = true;
+            this.webPageContentTb.Location = new System.Drawing.Point(12, 29);
+            this.webPageContentTb.Multiline = true;
+            this.webPageContentTb.Name = "webPageContentTb";
+            this.webPageContentTb.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.webPageContentTb.Size = new System.Drawing.Size(730, 535);
+            this.webPageContentTb.TabIndex = 0;
+            // 
+            // readBtn
+            // 
+            this.readBtn.Location = new System.Drawing.Point(667, 3);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(75, 23);
+            this.readBtn.TabIndex = 1;
+            this.readBtn.Text = "Read page";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.readBtn_Click);
+            // 
+            // urlTb
+            // 
+            this.urlTb.Location = new System.Drawing.Point(12, 3);
+            this.urlTb.Name = "urlTb";
+            this.urlTb.Size = new System.Drawing.Size(649, 20);
+            this.urlTb.TabIndex = 2;
+            this.urlTb.Text = "https://www.betclic.fr/football/ligue-1-e4";
+            // 
+            // footballBetclicBtn
+            // 
+            this.footballBetclicBtn.Location = new System.Drawing.Point(748, 3);
+            this.footballBetclicBtn.Name = "footballBetclicBtn";
+            this.footballBetclicBtn.Size = new System.Drawing.Size(106, 23);
+            this.footballBetclicBtn.TabIndex = 3;
+            this.footballBetclicBtn.Text = "Betclic football";
+            this.footballBetclicBtn.UseVisualStyleBackColor = true;
+            this.footballBetclicBtn.Click += new System.EventHandler(this.footballBetclicBtn_Click);
+            // 
+            // ParserFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 586);
+            this.Controls.Add(this.footballBetclicBtn);
+            this.Controls.Add(this.urlTb);
+            this.Controls.Add(this.readBtn);
+            this.Controls.Add(this.webPageContentTb);
+            this.Name = "ParserFrm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox webPageContentTb;
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.TextBox urlTb;
+        private System.Windows.Forms.Button footballBetclicBtn;
+    }
+}
+
---
+++ ArbMaker/ArbMaker/ParserFrm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using WebTools;
+using System.Windows.Forms;
+using WebTools.HtmlContent;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ArbMaker
+{
+    public partial class ParserFrm : Form
+    {
+        public ParserFrm()
+        {
+            InitializeComponent();
+        }
+
+        private async void readBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task<string> tsk = WebPageReader.ReadPage(urlTb.Text);
+                webPageContentTb.Text = await tsk;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void footballBetclicBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string content = webPageContentTb.Text;
+                string[] days = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+                string[] months = new string[] { "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre" };
+                string[] lines = Regex.Split(content, "\r\n");
+                //string[] lines = content.Split('\n');
+                int startingRowIdx = Array.FindIndex(lines, line => line.Contains("1 Nul 2")), size = lines.Length - startingRowIdx;
+                string[] rows = new string[size];
+                Array.Copy(lines, startingRowIdx, rows, 0, size);
+                rows.Replace("1 Nul 2", string.Empty);
+                DateTime date = DateTime.MinValue, currentDate = DateTime.MinValue;
+                int nbTeams = 2;
+                List<Odd> odds = new List<Odd>();
+
+                for(int i = 0; i < rows.Length; i++)
+                {
+                    string row = rows[i];
+                    row = row.Replace("Plus de stats ", string.Empty);
+                    if (row == " " || row == "paris") continue;
+                    else if (days.Any(day => row.Contains(day))) // date
+                    {
+                        string[] dateStr = Regex.Split(row, " ");
+                        int startDateFieldIdx = 0;
+                        while (dateStr[startDateFieldIdx] == string.Empty) startDateFieldIdx++;
+                        string monthStr = dateStr[startDateFieldIdx+2].ToLower();
+                        currentDate = date = new DateTime(DateTime.Today.Year, Array.FindIndex(months, month => month == monthStr) + 1, Convert.ToInt32(dateStr[startDateFieldIdx + 1]));
+                    }
+
+                    else if (row.Contains(":")) // time
+                    {
+                        date = currentDate.Add(TimeSpan.Parse(row));
+                    }
+
+                    else if (row.Contains("-")) // odds
+                    {
+                        string[] data = Regex.Split(row, " ");
+                        int startTeamFieldIdx = 0;
+                        while (data[startTeamFieldIdx] == string.Empty) startTeamFieldIdx++;
+
+                        string[] teams = new string[nbTeams];
+                        for(int k = 0; k < nbTeams; k++)
+                        {
+                            teams[k] = data[startTeamFieldIdx];
+                            int endTeamFieldIdx = startTeamFieldIdx + 1;
+                            while (data[endTeamFieldIdx] != "-" && !data[endTeamFieldIdx].Contains(","))
+                            {
+                                teams[k] = string.Format("{0} {1}", teams[k], data[endTeamFieldIdx]);
+                                endTeamFieldIdx++;
+                            }
+                            startTeamFieldIdx = endTeamFieldIdx + 1;
+                        }
+                        startTeamFieldIdx--;
+                        decimal win = Convert.ToDecimal(data[startTeamFieldIdx].Replace(",", ".")), draw = Convert.ToDecimal(data[startTeamFieldIdx + 1].Replace(",", ".")), loss = Convert.ToDecimal(data[startTeamFieldIdx + 2].Replace(",", "."));
+                        odds.Add(new Odd(date, teams[0], teams[1], win, loss, draw));
+                    }
+
+                    else // end stream
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Odd
+    {
+        private DateTime _timestamp;
+        private decimal _win, _loss, _draw;
+        private string _teamA, _teamB;
+
+        public Odd(DateTime timestamp, string teamA, string teamB, decimal win, decimal loss, decimal draw)
+        {
+            _timestamp = timestamp; _teamA = teamA; _teamB = teamB; _win = win; _draw = draw; _loss = loss;
+        }
+    }
+
+    public static class StringHelper
+    {
+        public static void Replace(this string[] array, string lookUp, string text)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = array[i].Replace(lookUp, text);
+        }
+    }
+}
